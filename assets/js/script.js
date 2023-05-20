6ddb7468afb923c3fd099bb9232b3bc916692692
Yajiit <tjagerter@gmail.com>
2023-05-20 11:50:37 -0400

    added uppcase/numbers/special functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 let includeLower;
 let includeUpper;
+let includeNumber;
+let includeSpecial;
+let length;
 function generatePassword() {
 // Promt for password length
   
@@ -9,24 +12,43 @@ function generatePassword() {
     length = parseInt(prompt("Ivalid- Please enter a length between 8 and 128:"));
   }
   // Prompt for which characters to include
-     includeLower = confirm("Include lowercase characters?");
-  
+     includeLower = confirm("Include lowercase letters?");
+     includeUpper = confirm("Include uppercase letters?");
+     includeNumber = confirm("Include numeric characters?");
+     includeSpecial = confirm("Include special characters?");
   // Defining available characters
-  const lowercase = "abcdefghijklmnopqrstuvwxyz";
-
+   const lowercase = "abcdefghijklmnopqrstuvwxyz";
+   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const numbers = "0123456789";
+   const specialChars = "!@#$%^&*()_-+=[]{}|;':\",./<>?";
   // Clears previous password and character set
-  let password = "";
-  let charSet = "";
+   let password = "";
+   let charSet = "";
 
   // Adding the available characters to password
-  if (includeLower) {
-    charSet += lowercase;
-    password += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+   if (includeLower) {
+     charSet += lowercase;
+     password += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+     length--;
+  }
+   if (includeUpper) {
+     charSet += uppercase;
+     password += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
+     length--;
+  }
+  if (includeNumber) {
+    charSet += numbers;
+    password += numbers.charAt(Math.floor(Math.random() * numbers.length));
     length--;
   }
-
-  for (let i = 0; i < length; i++) {
-    password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  if (includeSpecial) {
+    charSet += specialChars;
+    password += specialChars.charAt(Math.floor(Math.random() * specialChars.length));
+    length--;
+  }
+  // Filling the password with available characters
+   for (let i = 0; i < length; i++) {
+     password += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
 
     return password;
