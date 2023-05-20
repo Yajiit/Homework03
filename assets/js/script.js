f48face2b49a5c605f390b5667571830d52c3ba4
Yajiit <tjagerter@gmail.com>
2023-05-20 12:38:41 -0400

    Added prompt to quickly include all Alphanumeric characters at once. Added Shuffler function to decrease predictability of generated passwords

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,27 @@ let includeNumber;
 let includeSpecial;
 let length;
 function generatePassword() {
-// Promt for password length
-  
+// Ensures the alphanumeric sidestep prompt works properly on repeat generations
+  let includeAlphaNum = false;
+  // Promt for password length
     length = parseInt(prompt("Enter desired password length (between 8 and 128):"));
 //  Re-prompt for invalid inputs
     while (isNaN(length) || length < 8 || length > 128) {
     length = parseInt(prompt("Ivalid- Please enter a length between 8 and 128:"));
   }
-  // Prompt for which characters to include
+    // Prompt to add all alphanumeric with single ok
+    includeAlphaNum = confirm("Include all alphanumeric characters?")
+    if (includeAlphaNum) {
+      includeUpper = true;
+      includeLower = true;
+      includeNumber = true;
+    } else {
+  // Separate prompts for which characters to include
      includeLower = confirm("Include lowercase letters?");
      includeUpper = confirm("Include uppercase letters?");
      includeNumber = confirm("Include numeric characters?");
+    }
+  // Prompt for special characters
      includeSpecial = confirm("Include special characters?");
   // Defining available characters
    const lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -50,10 +60,19 @@ function generatePassword() {
    for (let i = 0; i < length; i++) {
      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
-
+  // Sends generated characters into shuffler function
+   password = shuffler(password);
     return password;
 }
-
+  // Function to shuffle the password to prevent the first four character types from being predictable
+function shuffler(string){
+  const array = string.split("");
+  for (let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i +1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.join("");
+}
 // Button to generate
 const generateBtn = document.getElementById("generateBtn");
 generateBtn.addEventListener("click", function() {
